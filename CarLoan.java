1fb6aff1c150e4e124e762dbaea1f18c08a0dd7c
grantkee <grant.d.kee@gmail.com>
2020-06-23 18:53:09 -0500

    Updated class to handle two decimal places.

--- CarLoan.java
+++ CarLoan.java
@@ -1,16 +1,21 @@
+import java.text.DecimalFormat;
+
 public class CarLoan {
-  int carLoan;
-  int loanLength;
-  int interestRate;
-  int downPayment;
+  double carLoan;
+  double loanLength;
+  double interestRate;
+  double downPayment;
+  double creditScore;
+  String cosigner;
 
 
   // constructor
-  public CarLoan(int loan, int duration, int apr, int initialPayment){
+  public CarLoan(double loan, double duration, double apr, double initialPayment, double credit){
     carLoan = loan;
     loanLength = duration;
     interestRate = apr;
     downPayment = initialPayment;
+    creditScore = credit;
   }
 
   public void calculatePayment(){
@@ -18,19 +23,23 @@ public class CarLoan {
       System.out.println("Error! You must take out a valid car loan.");
     } else if (downPayment >= carLoan){
       System.out.println("The car can be paid in full.");
+    } else if (creditScore <= 550 && cosigner == null){
+      System.out.println("You will need a cosigner for this loan.");
     } else {
-      int remainingBalance = carLoan - downPayment;
-      int months = loanLength * 12;
-      int monthlyBalance = remainingBalance / months;
-      int interest = monthlyBalance * interestRate / 100;
-      int monthlyPayment = monthlyBalance + interest;
-      System.out.println(monthlyPayment);
+      double remainingBalance = carLoan - downPayment;
+      double months = loanLength * 12;
+      double monthlyBalance = remainingBalance / months;
+      double interest = monthlyBalance * interestRate / 100;
+      double monthlyPayment = monthlyBalance + interest;
+      monthlyPayment = Math.round(monthlyPayment * 100);
+      monthlyPayment = monthlyPayment / 100;
+      System.out.println(monthlyPayment + " per month");
     }
   }
 
   // main method
 	public static void main(String[] args) {
-    CarLoan max = new CarLoan(10000, 3, 5, 2000);
+    CarLoan max = new CarLoan(10000, 3, 5, 2000, 555);
     max.calculatePayment();
 	}
 }
